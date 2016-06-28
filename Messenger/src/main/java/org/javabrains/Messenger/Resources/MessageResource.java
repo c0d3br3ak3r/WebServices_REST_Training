e60b6b34df4d146438425fb6bc069e77bf66f43b
Arshad <arshad4000@yahoo.co.in>
2016-06-28 10:46:36 +0530

    Added json supporot by modifying POM get resource by id

--- Messenger/src/main/java/org/javabrains/Messenger/Resources/MessageResource.java
+++ Messenger/src/main/java/org/javabrains/Messenger/Resources/MessageResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -22,10 +23,23 @@ public class MessageResource {
 		return msg_service.getAllMessages();
 	}
 	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{messageId}")
+	public MyMessage getMessage(@PathParam("messageId") long msgId) {
+		return msg_service.getMessage(msgId);
+	}
+	
 
 	@POST
 	@Produces(MediaType.TEXT_HTML)
 	public String getMessagesPost() {
 		return "<h1>Hello world</h1>";
+	}
+	
+	@GET
+	@Path("/test")
+	public String testMessage() {
+		return "This is just a test message";
 	}
 }
